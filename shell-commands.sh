cbbd5bbb9f752c9e7c02d82853c10c948a017827
Ubuntu <ubuntu@ip-172-31-81-33.ec2.internal>
2023-06-15 09:20:10 +0000

    java commit

--- shell-commands.sh
+++ shell-commands.sh
@@ -9,12 +9,12 @@ sudo apt install -y maven
 
 # Install Docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
